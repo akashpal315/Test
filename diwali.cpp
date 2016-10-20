038e8fb50f5f487df749886a28330129b3f2beb9
Akash <akashpal315@gmail.com>
2016-10-21 00:09:28 +0530

    Testing of GitHub Desktop Application
    Since I have started it during the Diwali Festival, so thought beginning
    with a simple greeting. :)

---
+++ diwali.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+void abc()
+{
+cout<<"* *\t\t* *";
+}
+void kis()
+{
+for(int i=0;i<5;i++)
+cout<<"* * ";
+}
+void xyz()
+{
+cout<<"* *\t\t\t* *";
+}
+void kis1()
+{
+for(int i=0;i<6;i++)
+cout<<"* * ";
+}
+int main()
+{
+
+for(int j=0;j;j++)
+{
+abc();
+cout<<endl;
+}
+for(int i=0;i<2;i++)
+{
+kis();
+cout<<"\n";
+}
+for(int i=0;i;i++)
+{
+abc();
+cout<<"\n";
+}
+abc();
+cout<<" APPY\n\n";
+for(int i=0;i<2;i++)
+{
+cout<<"\t\t\t";
+kis1();
+cout<<"\n";
+}
+for(int i=0;i<8;i++)
+{
+cout<<"\t\t\t ";
+xyz();
+cout<<"\n";
+}
+cout<<"\t\t\t";
+kis1();
+cout<<"\n";
+cout<<"\t\t\t";
+kis1();
+cout<<"\tIWALI";
+cout<<"\n\n\t\tFrom.­....Programming Solution.....In advance by - Mukesh chandra";
+return 0;
+}
